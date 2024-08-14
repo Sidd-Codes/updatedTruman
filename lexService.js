6b329ab578417d8ecd57b90b2a6b9dff7ebe2465
Sidd-Codes <107668103+Sidd-Codes@users.noreply.github.com>
2024-08-13 17:01:07 -0700

    Update lexService.js

--- lexService.js
+++ lexService.js
@@ -1,44 +1,29 @@
 const AWS = require('aws-sdk');
 
-// Configure AWS SDK
 AWS.config.update({
     region: process.env.AWS_REGION,
     accessKeyId: process.env.AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
 });
 
-// Create Lex runtime service object
 const lexRuntime = new AWS.LexRuntime();
 
-// Function to send message to Lex
-exports.sendMessageToLex = async (userId, message) => {
+exports.getLexBotResponse = async (intentName, slots = {}) => {
     const params = {
         botAlias: process.env.LEX_BOT_ALIAS,
         botName: process.env.LEX_BOT_NAME,
-        inputText: message,
-        userId: userId,
+        userId: 'LexBotActor',
+        inputText: intentName,
+        sessionAttributes: {
+            ...slots
+        }
     };
 
     try {
         const data = await lexRuntime.postText(params).promise();
-        return {
-            message: data.message,
-            intentName: data.intentName,
-            slots: data.slots
-        };
+        return data.message;
     } catch (error) {
         console.error('Error communicating with Lex:', error);
-        throw error;
-    }
-};
-
-// Function to get Lex bot response
-exports.getLexBotResponse = async (userId, message) => {
-    try {
-        const lexResponse = await this.sendMessageToLex(userId, message);
-        return lexResponse.message;
-    } catch (error) {
-        console.error('Error getting Lex bot response:', error);
-        return 'Sorry, I encountered an error. Please try again later.';
+        return 'An error occurred while generating content.';
     }
 };
